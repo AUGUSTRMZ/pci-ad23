95faa6cca0089990c97073f1a1fbb046c265716d
AUGUSTRMZ <142357102+AUGUSTRMZ@users.noreply.github.com>
2023-08-15 16:37:32 -0600

    ejemplo de practico sobre el uso de python

---
+++ validar precio.py
@@ -0,0 +1,11 @@
+# float lo convierte en un numero decimal
+precio = float(input("introduce precio: "))
+mensaje = ""  # se puede introducir una variable vacia para usarlo en lugar de print
+print(f"precio: ${precio}")
+if precio < 0:
+    print("el precio no es valido")
+    mensaje = "el precio no es valido"
+else:
+    print("el precio es valido")
+    mensaje = "el precio es valido"
+print(mensaje)
