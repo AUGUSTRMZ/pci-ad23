3802fec3f133f8583123bca12d89ea4a2bc6c355
AUGUSTRMZ <142357102+AUGUSTRMZ@users.noreply.github.com>
2023-09-03 15:54:50 -0600

    Add files via upload

--- FIFOODv0.1.py
+++ FIFOODv0.1.py
@@ -1,33 +1,47 @@
-print("Hola!!!, bienvenido a FIFOOD")
-print("Porfavor, ingresa los siguientes datos")
+def Dime_tu_peso():
+    peso = float(input("introduce tu peso en KG: "))
+    return peso
 
-# pedir los parametros al usuario
-peso = float(input("Introduce tu peso en KG: "))
-estatura = int(input("introduce tu estatura en cm ej.170: "))
-if estatura > 300:
-    print("esta altura es irreal porfavor ingresa datos reales")
-    mensaje = "reinicia el programa para volver a introducir tus datos"
-    print(mensaje)
-edad = int(input("por favor introduce tu edad: "))
-print("asegurate de que tus datos sean correctos")
-# definir la constante
-tmb = 65.5
-print("Muy bien, comencemos!!!")
 
-# definir las kcal diarias, estos calculos fueron hechos acorde a formulas que encontre en internet y otras que ya conocia
-consumo_calorias = tmb+13.75*peso+5.003*estatura-6.75*edad
-print(f"tus calorias a consumir son {consumo_calorias}kcal.")
+def Dime_tu_estatura():
+    estatura = float(input("introduce tu estatura en cm: "))
+    return estatura
 
-# definir los macronutrientes
-req_proteina = 1.6*peso
-print(f"deberias consumir aprox {req_proteina}g de proteina.")
 
-# para calcular las grasas se requieren 2 calculos para los cuales decidi divir la op en 2 para agregar mas complejidad al programa
-kcal_grasas = consumo_calorias*0.25
-req_grasas = kcal_grasas/9
-print(f"deberias consumir aprox {req_grasas}g de grasas ")
+def calcular_calorias():
 
-# definimos el ultimo macronutriente que es el mas sencillo
-req_carbohidratos = consumo_calorias/4
-print(f" deberias consumir aprox {req_carbohidratos}g de carbohidratos")
-print("gracias por usar la app")
+    tmb = 65.5
+    edad = int(input("por favor introduce tu edad: "))
+    calorias_diarias = tmb+13.75*Dime_tu_peso()+5.003*Dime_tu_estatura()-6.75*edad
+    return calorias_diarias
+
+
+def calcular_proteina():
+    proteina_diaria = 1.6*Dime_tu_peso()
+    return proteina_diaria
+
+
+def calcular_grasas():
+    calorias_grasas = calcular_calorias()*0.25
+    req_grasas = calorias_grasas/9
+    return req_grasas
+
+
+def calcular_carbohidratos():
+    carbos_diarios = calcular_calorias()/4
+    return carbos_diarios
+
+
+def main():
+    nombre = input("Hola cual es tu nombre?: ")
+    print(f"Bienvenido {nombre}!!!")
+    print("Muy bien, comencemos!!!")
+    print(f"tus calorias a consumir son {calcular_calorias()}kcal")
+    print(f"tu requerimiento de proteina diaria es de {calcular_proteina()}g")
+    print(f"tu requerimiento de grasas diaria es de {calcular_grasas()}g")
+    print(
+        f"tu requerimiento de carbohidratos es de {calcular_carbohidratos()}g")
+    print(f"MUCHAS GRACIAS {nombre} nos vemos pronto")
+
+
+main()
